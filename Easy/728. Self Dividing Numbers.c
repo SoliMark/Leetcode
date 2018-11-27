5485a32ff965a8debeac018024aa89b02f592a0f
SoliMark <zzzxxx804113401@gmail.com>
2018-11-28 00:05:42 +0800

    728 some code revise

--- Easy/728. Self Dividing Numbers.c
+++ Easy/728. Self Dividing Numbers.c
@@ -4,29 +4,23 @@
  */
 int* selfDividingNumbers(int left, int right, int* returnSize) {
     int i;
-    int *ret;
-    ret = malloc((*returnSize = 0) * sizeof(int));
-    for(i=left;i<=right;++i){
-        if (i%10!=0){
-            int digit=i;
-            int c=1;
-            while(digit){
-                
-                if((digit%10==0)||(i%(digit%10)!=0)){
+    
+    int *ret = malloc((*returnSize = 0) * sizeof(int));
+    for(i=left;i<=right;++i){       
+        int digit=i;
+        int c=1;
+        while(digit){   
+            if((digit%10==0)||(i%(digit%10)!=0)){
                     c=0;
                 }
                 digit/=10;
-            }
-            if(c){
-                ret = realloc(ret, ++*returnSize * sizeof(int));
-			    ret[*returnSize - 1] = i;
-                
-            }
         }
-            
+        if(c){
+            ret = realloc(ret, ++*returnSize * sizeof(int));
+			ret[*returnSize - 1] = i;
+                
+        }                   
     }
-    
-    
     return ret ;
     
 }
