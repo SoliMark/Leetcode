620b72bd1cfe6bf0cbdae943d84a3a965fdb5145
SoliMark <zzzxxx804113401@gmail.com>
2018-11-28 00:00:26 +0800

    ADD 728

---
+++ Easy/728. Self Dividing Numbers.c
@@ -0,0 +1,32 @@
+/**
+ * Return an array of size *returnSize.
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* selfDividingNumbers(int left, int right, int* returnSize) {
+    int i;
+    int *ret;
+    ret = malloc((*returnSize = 0) * sizeof(int));
+    for(i=left;i<=right;++i){
+        if (i%10!=0){
+            int digit=i;
+            int c=1;
+            while(digit){
+                
+                if((digit%10==0)||(i%(digit%10)!=0)){
+                    c=0;
+                }
+                digit/=10;
+            }
+            if(c){
+                ret = realloc(ret, ++*returnSize * sizeof(int));
+			    ret[*returnSize - 1] = i;
+                
+            }
+        }
+            
+    }
+    
+    
+    return ret ;
+    
+}
