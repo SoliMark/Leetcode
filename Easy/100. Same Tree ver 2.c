0024cec5e97185dda7a422438f093d4c03c3c9fa
SoliMark <zzzxxx804113401@gmail.com>
2019-07-26 21:47:44 +0800

    100. Same Tree

---
+++ Easy/100. Same Tree ver 2.c
@@ -0,0 +1,6 @@
+bool isSameTree(struct TreeNode* p, struct TreeNode* q){
+    
+    if(!p&&!q) return true;
+    if(!p||!q) return false;
+    return p->val==q->val&&isSameTree( p->right, q->right)&&isSameTree( p->left, q->left);
+}
