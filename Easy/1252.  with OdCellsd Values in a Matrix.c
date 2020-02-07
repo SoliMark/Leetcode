5fb59fbbd8f4ec4e0fabdf4c1ce0657d0bd1f92c
SoliMark <zzzxxx804113401@gmail.com>
2020-02-07 21:31:54 +0800

    Create 1252.  with OdCellsd Values in a Matrix.c

---
+++ Easy/1252.  with OdCellsd Values in a Matrix.c
@@ -0,0 +1,47 @@
+void addrc(int **matrix,int size ,int index,int rc){
+    /*
+    r==0;
+    c==1    
+    */
+    if(rc ==0){
+        for(int i=0;i<size;++i){
+            matrix[index][i]++;
+        }
+    }else{
+        for(int i=0;i<size;++i){
+            matrix[i][index]++;
+        }
+    }
+    
+}
+
+
+
+int oddCells(int n, int m, int** indices, int indicesSize, int* indicesColSize){
+    int *matrix[n];
+    int ret =0;
+    *indicesColSize=2;
+    
+    for(int i=0;i<n;++i){
+        matrix[i] =(int *)malloc (sizeof(int)*m);
+    }
+    
+    for(int i=0;i<n;++i){
+        for(int j=0;j<m;++j){
+            matrix[i][j]=0;
+        }
+    }
+    
+    for(int i=0;i<indicesSize;++i){
+        addrc(matrix,m,indices[i][0],0);
+        addrc(matrix,n,indices[i][1],1);
+    }
+    for(int i=0;i<n;++i){
+        for(int j=0;j<m;++j){
+            if(matrix[i][j]%2==1) ret ++;
+        }
+    }
+    return ret ;
+}
+
+
